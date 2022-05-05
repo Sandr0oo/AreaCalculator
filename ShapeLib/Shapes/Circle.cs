6fef856d67767b7bf071d70142ab90f348a4600e
Sandr0oo <sandro88steel@mail.ru>
2022-05-05 09:31:37 +0300

    В фигуры добавлены свойства

--- ShapeLib/Shapes/Circle.cs
+++ ShapeLib/Shapes/Circle.cs
@@ -4,18 +4,18 @@ namespace ShapeLib
 {
     public class Circle : IShape
     {
-        private double _radius;
+        public double Radius { get; private set; }
 
         public Circle(double radius)
         {
             if (radius <= 0)
                 throw new Exception("Радиус должен быть больше нуля");
-            _radius = radius;
+            Radius = radius;
         }
 
         public double GetArea()
         {
-            return Math.PI * Math.Pow(_radius, 2);
+            return Math.PI * Math.Pow(Radius, 2);
         }
     }
 }
--- ShapeLib/Shapes/Triangle.cs
+++ ShapeLib/Shapes/Triangle.cs
@@ -4,34 +4,34 @@ namespace ShapeLib
 {
     public class Triangle : IShape
     {
-        private double _a;
-        private double _b;
-        private double _c;
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
 
         public Triangle(double a, double b, double c)
         {
-            if (a < 0 || b < 0 || c < 0)
-                throw new Exception("Сторона тругольника не может быть меньше нуля.");
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new Exception("Сторона тругольника не может быть равна нулю или быть меньше нуля.");
 
             bool isTringleExist = a + b > c && b + c > a && a + c > b;
             if (!isTringleExist)
                 throw new Exception("Такой треугольник не существует");
 
-            _a = a;
-            _b = b;
-            _c = c;
+            A = a;
+            B = b;
+            C = c;
         }
 
         public double GetArea()
         {
-            double halfPerimetr = (_a + _b + _c) / 2;
-            double area = halfPerimetr * (halfPerimetr - _a) * (halfPerimetr - _b) * (halfPerimetr - _c);
+            double halfPerimetr = (A + B + C) / 2;
+            double area = halfPerimetr * (halfPerimetr - A) * (halfPerimetr - B) * (halfPerimetr - C);
             return Math.Sqrt(area);
         }
 
         public bool IsRightTriangle()
         {
-            double[] sidesOfTringle = new double[] { _a, _b, _c };
+            double[] sidesOfTringle = new double[] { A, B, C };
             Array.Sort(sidesOfTringle);
             return Math.Pow(sidesOfTringle[2], 2) == Math.Pow(sidesOfTringle[1], 2) + Math.Pow(sidesOfTringle[0], 2);
         }
